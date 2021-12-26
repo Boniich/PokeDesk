65c7bb16b641498c7f10b7b9662b0de5ee731318
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-12-26 12:32:52 -0300

    this function allows print all cards

--- app/component/cards/Cards.js
+++ app/component/cards/Cards.js
@@ -1,12 +1,13 @@
 function Cards(props){
 
     let {cards} = props;
-
+    let length = cards.length;
 
     const $card = document.createElement("div");
 
-    $card.innerHTML = `<img width = "250px" src="${cards[0].img}"></img>`;
-
+    for(let e = 0; e<length;e++){
+        $card.innerHTML += `<img width = "250px" src="${cards[e].img}"></img>`;
+    }
 
     return $card;
 }
