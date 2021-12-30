2669f722423c8b55f4f596b88b093cbf05fd71fe
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-12-30 15:23:37 -0300

    added desk section y cards section

--- app/App.js
+++ app/App.js
@@ -1,11 +1,15 @@
+import CardSection from "./component/cardSection/CardSection.js";
+import DeskSection from "./component/deskSection/DeskSection.js";
 import Nav from "./component/nav/Nav.js";
 
 function App(){
     const $root = document.getElementById("root");
-
+    
     $root.innerHTML = null;
 
     $root.appendChild(Nav());
+    $root.appendChild(DeskSection());
+    $root.appendChild(CardSection());
 
 }
 
