843185e2de0a5e4e5fbc7646e62d6d3b8abf4f4b
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-12-30 13:28:13 -0300

    added a div for each img and css class for div container of function

--- app/component/cards/Cards.js
+++ app/component/cards/Cards.js
@@ -4,9 +4,14 @@ function Cards(props){
     let length = cards.length;
 
     const $card = document.createElement("div");
+    $card.classList.add("cards-grid");
 
     for(let e = 0; e<length;e++){
-        $card.innerHTML += `<img class="cards" width = "250px" src="${cards[e].img}"></img>`;
+        
+        $card.innerHTML += `
+        <div class="cards">
+        <img width = "250px" src="${cards[e].img}"></img>
+        </div>`;
     }
 
     return $card;
